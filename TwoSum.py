75bbb8c7363dad7216f522c6f5737e1a18f15b8a
Venkat <venkat199914@gmail.com>
2025-12-25 20:34:14 -0500

    Two Sum

---
+++ TwoSum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+
+        prevMap = {}
+
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return 
+    
+nums = [4,5,6]
+target = 10
+sol = Solution()
+print(sol.twoSum(nums,target))
